ce46960ea69351b549041e1aa4d6d9595556d13a
sarthakahuja11 <sarthakahuja11@gmail.com>
2019-07-22 13:15:34 +0530

    test commit to #60daysUdacity file

--- Lesson2_NN_in_pytorch.py
+++ Lesson2_NN_in_pytorch.py
@@ -18,9 +18,7 @@ import matplotlib.pyplot as plt
 from torchvision import datasets, transforms
 
 # Define a transform to normalize the data
-transform = transforms.Compose([transforms.ToTensor(),
-                              transforms.Normalize((0.5,), (0.5,)),
-                              ])
+transform = transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.5,), (0.5,)), ])
 
 # Download and load the training data
 trainset = datasets.MNIST('~/.pytorch/MNIST_data/', download=True, train=True, transform=transform)
