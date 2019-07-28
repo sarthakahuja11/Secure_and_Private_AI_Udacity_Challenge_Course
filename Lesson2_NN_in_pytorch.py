799bc0584455991dcbecb2a2b8534df672d7f9f9
sarthakahuja11 <sarthakahuja11@gmail.com>
2019-07-28 21:52:45 +0530

    added day 18, with code files to #60daysUdacity file

--- Lesson2_NN_in_pytorch.py
+++ Lesson2_NN_in_pytorch.py
@@ -78,6 +78,7 @@ print(probabilities)
 ##################################
 #  NETWORK IMPORT NN
 
+
 from torch import nn
 
 class Network(nn.Module):
@@ -103,6 +104,25 @@ class Network(nn.Module):
         return x
 
 
+# Create the network and look at it's text representation
+model = Network()
+model
 
 
+import torch.nn.functional as F
 
+class Network(nn.Module):
+    def __init__(self):
+        super().__init__()
+        # Inputs to hidden layer linear transformation
+        self.hidden = nn.Linear(784, 256)
+        # Output layer, 10 units - one for each digit
+        self.output = nn.Linear(256, 10)
+        
+    def forward(self, x):
+        # Hidden layer with sigmoid activation
+        x = F.sigmoid(self.hidden(x))
+        # Output layer with softmax activation
+        x = F.softmax(self.output(x), dim=1)
+        
+        return x
